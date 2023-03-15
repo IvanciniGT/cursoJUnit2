c8b9db90d8ee8fffa331028104e0e7d9734bc055
Ubuntu <ubuntu@ip-172-31-12-218.eu-west-1.compute.internal>
2023-03-15 17:18:03 +0000

    instalacion real

--- proyectos/diccionario/src/test/java/com/curso/LoginTest.java
+++ proyectos/diccionario/src/test/java/com/curso/LoginTest.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.util.List;
 import java.net.URL;
 import java.util.Optional;
+import java.nio.file.*;
 
 import com.curso.impl.Utilidades;
 
@@ -30,15 +31,24 @@ public class LoginTest {
     driver = new RemoteWebDriver( new URL("http://34.243.13.64:4444/wd/hub"), capabilities );
   }
 
-  @Test
-  public void testProbarLoginExistoso() throws Exception {
+@ParameterizedTest
+@DisplayName("Comprobar pantalla de login con datos correctos")
+@CsvFileSource(resources="/datos_correctos_login.csv", numLinesToSkip=1 )
+  public void testProbarLoginExistoso(String usuario, String password) throws Exception {
     driver.get("https://katalon-demo-cura.herokuapp.com/profile.php#login");
     driver.findElement(By.id("txt-username")).clear();
-    driver.findElement(By.id("txt-username")).sendKeys("John Doe");
+    driver.findElement(By.id("txt-username")).sendKeys(usuario);
     driver.findElement(By.id("txt-password")).clear();
-    driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+    driver.findElement(By.id("txt-password")).sendKeys(password);
     driver.findElement(By.id("btn-login")).click();
     Assertions.assertEquals("Make Appointment", driver.findElement(By.xpath("//section[@id='appointment']//h2")).getText());
+    capturarPantalla(driver, "login_ok")
+  }
+
+  public static capturarPantalla(WebDriver driver, String nombre){
+      TakesScreenshot capturador=(TakesScreenshot)driver;
+      File imagen = capturador.getScreenshotAs(OutputType.FILE);
+      Files.copy(imagen.toPath(), Paths.get("./"+nombre+".png"), StandardCopyAction.REPLACE_EXISTING);
   }
 
   @AfterAll
